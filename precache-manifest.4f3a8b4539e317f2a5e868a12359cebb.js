323349b4fb0a7d9ca997728726b29ab086606aa4
Bianca Andrade <emi.andrade37@hotmail.com>
2023-07-05 12:23:45 -0300

    Updates

---
+++ precache-manifest.4f3a8b4539e317f2a5e868a12359cebb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f85d8e852c23715bfc4",
+    "url": "/Projeto-Youtube/static/css/main.62015c2c.chunk.css"
+  },
+  {
+    "revision": "2f85d8e852c23715bfc4",
+    "url": "/Projeto-Youtube/static/js/main.2f85d8e8.chunk.js"
+  },
+  {
+    "revision": "27d2720eecac80dec00b",
+    "url": "/Projeto-Youtube/static/js/1.27d2720e.chunk.js"
+  },
+  {
+    "revision": "4ec99a702fa343f3e04c",
+    "url": "/Projeto-Youtube/static/js/runtime~main.4ec99a70.js"
+  },
+  {
+    "revision": "878c7769f3435fc9f2625967b25129cf",
+    "url": "/Projeto-Youtube/static/media/search.878c7769.png"
+  },
+  {
+    "revision": "f837cb25f527e93a43d98bbb4f6dbb1c",
+    "url": "/Projeto-Youtube/static/media/sino.f837cb25.png"
+  },
+  {
+    "revision": "eebc1a6efca2f0344e1668894adfd37b",
+    "url": "/Projeto-Youtube/static/media/ajuda.eebc1a6e.png"
+  },
+  {
+    "revision": "e0651ac1cbea892e47cbb20777f434e5",
+    "url": "/Projeto-Youtube/static/media/google.e0651ac1.png"
+  },
+  {
+    "revision": "f128cf2c34351f5854d474d3e8a53f05",
+    "url": "/Projeto-Youtube/index.html"
+  }
+];
